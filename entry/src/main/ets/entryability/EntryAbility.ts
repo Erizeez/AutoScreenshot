bc20d7a06c0a73def700052a592326e27fe3b607
Zejiong Dong <810918843@qq.com>
2023-04-30 17:15:19 +0800

    test

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,10 +1,27 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
+import abilityAccessCtrl from '@ohos.abilityAccessCtrl';
+import type { Permissions } from '@ohos.abilityAccessCtrl';
 
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+    hilog.info(0x0000,'testTag','EntryAbility onCreate')
+    globalThis.abilityWant = want;
+    globalThis.shotScreenContext = this.context;
+    //The list of user_grant permission, MEDIA|MIC separated to grant
+    // mediaLibrary接口被废弃，新接口需要ohos.permission.READ_IMAGEVIDEO权限，测试用例已经升级到新接口，sample未处理
+    const PERMISSIONS: Array<Permissions> = [
+      'ohos.permission.MEDIA_LOCATION',
+      'ohos.permission.READ_MEDIA',
+      'ohos.permission.WRITE_MEDIA',
+      'ohos.permission.MICROPHONE',
+      'ohos.permission.READ_IMAGEVIDEO',
+      'ohos.permission.CAPTURE_SCREEN',
+      'ohos.permission.SYSTEM_FLOAT_WINDOW'
+    ];
+    let atManager: abilityAccessCtrl.AtManager = abilityAccessCtrl.createAtManager();
+    atManager.requestPermissionsFromUser(this.context, PERMISSIONS);
   }
 
   onDestroy() {
@@ -21,6 +38,15 @@ export default class EntryAbility extends UIAbility {
         return;
       }
       hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+    });
+
+    windowStage.getMainWindow((err, data) => {
+      if (err.code) {
+        hilog.error(0x0000,'testTag', 'Failed to obtain the main window. Cause: ' + JSON.stringify(err));
+        return;
+      }
+      globalThis.mainWindow = data;
+      hilog.error(0x0000,'testTag', 'Succeeded in obtaining the main window. Data: ' + JSON.stringify(data));
     });
   }
 
